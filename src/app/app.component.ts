d1340d01bf2ed6252d5630e79a1937944715b2a6
daniel-aragao <danielfilhoce@gmail.com>
2022-03-07 02:12:48 -0500

    adding particles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import { Grid } from 'src/app/interfaces/models/grid';
 import { SimulationController } from './models/simulationController';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Output } from './interfaces/models/output';
+
+declare var particlesJS: {
+  load: (elementId: string, configPath: string, callback: () => void) => void;
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   private simulation: SimulationController;
   outputs: Output[] = [];
 
@@ -29,6 +33,15 @@ export class AppComponent {
    */
   constructor() {
     this.simulation = new SimulationController();
+  }
+  ngOnInit(): void {
+    particlesJS.load(
+      'particle-container',
+      'assets/particlesjs-config.json',
+      function () {
+        console.log('callback - particles.js config loaded');
+      }
+    );
   }
 
   reset() {
